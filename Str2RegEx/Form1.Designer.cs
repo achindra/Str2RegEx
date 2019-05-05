b06311bdb2bfccbc0459d166fdd8f5c6901e7e66
Achindra Bhatnagar <achindra@github.com>
2019-05-05 14:56:23 +0530

    String to RegEx

---
+++ Str2RegEx/Form1.Designer.cs
@@ -0,0 +1,291 @@
+﻿namespace Str2RegEx
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.rtbOpenFile = new System.Windows.Forms.RichTextBox();
+            this.rtbRegEx = new System.Windows.Forms.RichTextBox();
+            this.txtLinesToSample = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.rtbFrequency = new System.Windows.Forms.RichTextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.lblCluster = new System.Windows.Forms.Label();
+            this.lblHash = new System.Windows.Forms.Label();
+            this.lblFrequency = new System.Windows.Forms.Label();
+            this.btnTrain = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.samplerDataSet = new Str2RegEx.SamplerDataSet();
+            this.sampleDBBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.sampleDBTableAdapter = new Str2RegEx.SamplerDataSetTableAdapters.SampleDBTableAdapter();
+            this.clusterDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.hashDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.frequencyDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.stackDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.rtbPattern = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.samplerDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sampleDBBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(544, 15);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load File";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.openFileDialog1_FileOk);
+            // 
+            // rtbOpenFile
+            // 
+            this.rtbOpenFile.Location = new System.Drawing.Point(12, 49);
+            this.rtbOpenFile.Name = "rtbOpenFile";
+            this.rtbOpenFile.Size = new System.Drawing.Size(882, 356);
+            this.rtbOpenFile.TabIndex = 1;
+            this.rtbOpenFile.Text = "";
+            // 
+            // rtbRegEx
+            // 
+            this.rtbRegEx.Location = new System.Drawing.Point(900, 49);
+            this.rtbRegEx.Name = "rtbRegEx";
+            this.rtbRegEx.Size = new System.Drawing.Size(481, 356);
+            this.rtbRegEx.TabIndex = 2;
+            this.rtbRegEx.Text = "";
+            // 
+            // txtLinesToSample
+            // 
+            this.txtLinesToSample.AutoCompleteCustomSource.AddRange(new string[] {
+            "100",
+            "250",
+            "500",
+            "750",
+            "1000"});
+            this.txtLinesToSample.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.txtLinesToSample.Location = new System.Drawing.Point(428, 14);
+            this.txtLinesToSample.Name = "txtLinesToSample";
+            this.txtLinesToSample.Size = new System.Drawing.Size(100, 20);
+            this.txtLinesToSample.TabIndex = 4;
+            this.txtLinesToSample.Text = "100";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(339, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Lines to sample:";
+            // 
+            // rtbFrequency
+            // 
+            this.rtbFrequency.Location = new System.Drawing.Point(439, 411);
+            this.rtbFrequency.Name = "rtbFrequency";
+            this.rtbFrequency.Size = new System.Drawing.Size(420, 295);
+            this.rtbFrequency.TabIndex = 6;
+            this.rtbFrequency.Text = "";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.clusterDataGridViewTextBoxColumn,
+            this.hashDataGridViewTextBoxColumn,
+            this.frequencyDataGridViewTextBoxColumn,
+            this.stackDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.sampleDBBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(876, 411);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(505, 290);
+            this.dataGridView1.TabIndex = 8;
+            // 
+            // lblCluster
+            // 
+            this.lblCluster.AutoSize = true;
+            this.lblCluster.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblCluster.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lblCluster.Location = new System.Drawing.Point(674, 13);
+            this.lblCluster.Name = "lblCluster";
+            this.lblCluster.Size = new System.Drawing.Size(12, 15);
+            this.lblCluster.TabIndex = 9;
+            this.lblCluster.Text = ":";
+            // 
+            // lblHash
+            // 
+            this.lblHash.AutoSize = true;
+            this.lblHash.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblHash.Location = new System.Drawing.Point(900, 13);
+            this.lblHash.Name = "lblHash";
+            this.lblHash.Size = new System.Drawing.Size(12, 15);
+            this.lblHash.TabIndex = 10;
+            this.lblHash.Text = ":";
+            // 
+            // lblFrequency
+            // 
+            this.lblFrequency.AutoSize = true;
+            this.lblFrequency.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lblFrequency.Location = new System.Drawing.Point(1167, 13);
+            this.lblFrequency.Name = "lblFrequency";
+            this.lblFrequency.Size = new System.Drawing.Size(12, 15);
+            this.lblFrequency.TabIndex = 11;
+            this.lblFrequency.Text = ":";
+            // 
+            // btnTrain
+            // 
+            this.btnTrain.Enabled = false;
+            this.btnTrain.Location = new System.Drawing.Point(142, 13);
+            this.btnTrain.Name = "btnTrain";
+            this.btnTrain.Size = new System.Drawing.Size(75, 23);
+            this.btnTrain.TabIndex = 12;
+            this.btnTrain.Text = "Train DB";
+            this.btnTrain.UseVisualStyleBackColor = true;
+            this.btnTrain.Click += new System.EventHandler(this.btnTrain_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(22, 13);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(114, 17);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Train From Sample";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // samplerDataSet
+            // 
+            this.samplerDataSet.DataSetName = "SamplerDataSet";
+            this.samplerDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // sampleDBBindingSource
+            // 
+            this.sampleDBBindingSource.DataMember = "SampleDB";
+            this.sampleDBBindingSource.DataSource = this.samplerDataSet;
+            // 
+            // sampleDBTableAdapter
+            // 
+            this.sampleDBTableAdapter.ClearBeforeFill = true;
+            // 
+            // clusterDataGridViewTextBoxColumn
+            // 
+            this.clusterDataGridViewTextBoxColumn.DataPropertyName = "Cluster";
+            this.clusterDataGridViewTextBoxColumn.HeaderText = "Cluster";
+            this.clusterDataGridViewTextBoxColumn.Name = "clusterDataGridViewTextBoxColumn";
+            // 
+            // hashDataGridViewTextBoxColumn
+            // 
+            this.hashDataGridViewTextBoxColumn.DataPropertyName = "Hash";
+            this.hashDataGridViewTextBoxColumn.HeaderText = "Hash";
+            this.hashDataGridViewTextBoxColumn.Name = "hashDataGridViewTextBoxColumn";
+            // 
+            // frequencyDataGridViewTextBoxColumn
+            // 
+            this.frequencyDataGridViewTextBoxColumn.DataPropertyName = "Frequency";
+            this.frequencyDataGridViewTextBoxColumn.HeaderText = "Frequency";
+            this.frequencyDataGridViewTextBoxColumn.Name = "frequencyDataGridViewTextBoxColumn";
+            // 
+            // stackDataGridViewTextBoxColumn
+            // 
+            this.stackDataGridViewTextBoxColumn.DataPropertyName = "Stack";
+            this.stackDataGridViewTextBoxColumn.HeaderText = "Stack";
+            this.stackDataGridViewTextBoxColumn.Name = "stackDataGridViewTextBoxColumn";
+            // 
+            // rtbPattern
+            // 
+            this.rtbPattern.Location = new System.Drawing.Point(12, 411);
+            this.rtbPattern.Name = "rtbPattern";
+            this.rtbPattern.Size = new System.Drawing.Size(420, 294);
+            this.rtbPattern.TabIndex = 14;
+            this.rtbPattern.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1387, 709);
+            this.Controls.Add(this.rtbPattern);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.btnTrain);
+            this.Controls.Add(this.lblFrequency);
+            this.Controls.Add(this.lblHash);
+            this.Controls.Add(this.lblCluster);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.rtbFrequency);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtLinesToSample);
+            this.Controls.Add(this.rtbRegEx);
+            this.Controls.Add(this.rtbOpenFile);
+            this.Controls.Add(this.btnLoad);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.samplerDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.sampleDBBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.RichTextBox rtbOpenFile;
+        private System.Windows.Forms.RichTextBox rtbRegEx;
+        private System.Windows.Forms.TextBox txtLinesToSample;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox rtbFrequency;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label lblCluster;
+        private System.Windows.Forms.Label lblHash;
+        private System.Windows.Forms.Label lblFrequency;
+        private System.Windows.Forms.Button btnTrain;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private SamplerDataSet samplerDataSet;
+        private System.Windows.Forms.BindingSource sampleDBBindingSource;
+        private SamplerDataSetTableAdapters.SampleDBTableAdapter sampleDBTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn clusterDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn hashDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn frequencyDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn stackDataGridViewTextBoxColumn;
+        private System.Windows.Forms.RichTextBox rtbPattern;
+    }
+}
+
---
+++ Str2RegEx/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Data.OleDb;
+using System.Data.SqlServerCe;
+using System.Data.Sql;
+using System.Data.SqlClient;
+
+namespace Str2RegEx
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class DistributionList
+        {
+            public string Line { get; set; }
+            public uint Frequency { get; set; }
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+            MineThroughText(openFileDialog1.FileName, "");
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            rtbOpenFile.Text = "";
+            rtbRegEx.Text = "";
+            lblCluster.Text = lblFrequency.Text = lblHash.Text = ":";
+            openFileDialog1.ShowDialog();
+        }
+
+        private string ConvertRegEx(string Token)
+        {
+            Token.Trim();
+
+            if (Regex.Match(Token, @"^[\d]{2}[:][\d]{2}[:][\d]{2}").Success)
+                return "Time";
+            //dd/mm/yyyy
+            else if (Regex.Match(Token, @"^\b(0?[1-9]|[12][0-9]|3[01])[- /.](0?[1-9]|1[012])[- /.](19|20)?[0-9]{2}\b").Success)
+                return "Date";
+            //mm/dd/yyyy
+            else if (Regex.Match(Token, @"^\b(0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])[- /.](19|20)?[0-9]{2}\b").Success)
+                return "Date";
+            //yyyy-mm-dd
+            else if (Regex.Match(Token, @"^\b(19|20)?[0-9]{2}[- /.](0?[1-9]|1[012])[- /.](0?[1-9]|[12][0-9]|3[01])\b").Success)
+                return "Date";
+            //else if (Regex.Match(Token, @"^[\d]{4}[-][\d]{2}[-][\d]{2}$").Success)
+            //    return "Date";
+            //else if (Regex.Match(Token.ToLower(), @"^[^@]*([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})").Success)
+            else if (Regex.Match(Token, @"^\b[a-fA-F0-9]{8}(?:-[a-fA-F0-9]{4}){3}-[a-fA-F0-9]{12}\b").Success)
+                return "GUID";
+            //else if (Regex.Match(Token, @"\b([A-Za-z0-9]+(-[A-Za-z0-9]+)*\.)+[A-Za-z]{2,}\b").Success)
+            //else if (Regex.Match(Token, @"([A-Z0-9a-z_-]+[\.][A-Z0-9a-z_-]{2,6})$").Success)
+            else if (Regex.Match(Token, @"^([0-9A-Fa-f])\:([0-9A-Fa-f])").Success)
+                return "PID:TID";
+            else if (Regex.Match(Token, @"^[0-9A-Za-z_-~]*(\.[0-9A-Za-z_-~]{2,6})$").Success)
+                return "File";
+            else if (Regex.Match(Token, @"([a-zA-Z]\:)(\\[^\\/:~*?<>|]*(?<![ ]))*(\.[A-Z0-9a-z_-]{2,6})$").Success)
+                return "FilePath";
+            else if (Regex.Match(Token, @"([a-zA-Z]\:\\)([^\\/:~*?<>|]*(?<![ ]))*").Success)
+                return "Path";
+            else if (Regex.Match(Token, @"\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}\b").Success)
+                return "EMail";
+            else if (Regex.Match(Token, @"\b[a-f0-9A-F]{2}([-:]?)(?:[a-f0-9A-F]{2}\1){4}[a-f0-9A-F]{2}\b").Success)
+                return "MAC";
+            else if (Regex.Match(Token, @"\b0[xX][0-9a-fA-F]+\b").Success)
+                return "Hex";
+            //else if (Regex.Match(Token, @"/([1-9][0-9]*)/").Success)
+            //    return "Num";
+            // +/-num.num (int and float)
+            else if (Regex.Match(Token, @"^(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\.(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$").Success)
+                return "IPv4";
+            else if (Regex.Match(Token, @"^[-+]?\b[0-9]*\.?[0-9]+\b").Success)
+                return "Num";
+            //else if (Regex.Match(Token, @"0[xX][A-Fa-f0-9]+").Success)
+            //    return "Hex";
+            else if (Regex.Match(Token, @"^[A-Z]+").Success) //^[A-Z_]");
+                return "CAP";
+            else if (Regex.Match(Token, @"[a-zA-Z_]").Success)
+                return "Text";
+            else if (Regex.Match(Token, @"^[a-zA-Z0-9]").Success)
+                return "AlphaNum";
+
+            return Token;
+        }
+
+        private void MineThroughText(string FilePath, string Cluster)
+        {
+            StreamReader Reader = new StreamReader(FilePath);
+            uint LineCounters = 0;
+            List<DistributionList> DistList = new List<DistributionList>();
+
+            if ("" == txtLinesToSample.Text.Trim())
+                txtLinesToSample.Text = "100";
+
+            while (!Reader.EndOfStream && LineCounters < Convert.ToInt32(txtLinesToSample.Text))
+            {
+                string pattern = "";
+                string LinePattern = "";
+                string strText = "";
+                string[] tokens;
+
+                // read line
+                strText = Reader.ReadLine();
+                LineCounters++;
+                rtbOpenFile.Text = rtbOpenFile.Text + "\n" + strText;
+
+                // string to token
+                tokens = strText.Split(new Char[] { ' ', ',', '\t' });
+
+                // Find pattern in each token
+                foreach (string token in tokens)
+                {
+                    if (token != "")
+                    {
+                        string tempPattern = ConvertRegEx(token);
+
+                        if (pattern != "Text")
+                        {
+                            pattern = tempPattern;
+                            LinePattern = LinePattern + " " + pattern;
+                        }
+                        else if (tempPattern != "Text")
+                        {
+                            pattern = tempPattern;
+                            LinePattern = LinePattern + " " + pattern;
+                        }
+                    }
+                }
+
+                rtbRegEx.Text = rtbRegEx.Text + "\n" + LinePattern;
+                DistributionList Line = DistList.Find(
+                    delegate(DistributionList DL)
+                    {
+                        return DL.Line == LinePattern;
+                    }
+                );
+
+                if (Line != null)
+                {
+                    Line.Frequency++;
+                }
+                else
+                {
+                    DistributionList Item = new DistributionList();
+                    Item.Line = LinePattern;
+                    Item.Frequency = 1;
+                    DistList.Add(Item);
+                }
+
+            }
+            Reader.Close();
+
+            rtbPattern.Text = rtbFrequency.Text = "";
+            //DistList.Sort();
+            foreach (DistributionList T in DistList)
+            {
+                rtbPattern.Text = rtbPattern.Text + "\n" + T.Line;// +"::" + T.Frequency;
+                rtbFrequency.Text = rtbFrequency.Text + "\n" + T.Line +"::" + T.Frequency;
+            }
+
+            string hash; 
+            string fhash;
+
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                hash = BitConverter.ToString(
+                  md5.ComputeHash(Encoding.UTF8.GetBytes(rtbFrequency.Text))
+                ).Replace("-", String.Empty);
+            }
+
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                fhash = BitConverter.ToString(
+                  md5.ComputeHash(Encoding.UTF8.GetBytes(rtbPattern.Text))
+                ).Replace("-", String.Empty);
+            }
+
+            //Check DB
+            SqlCeConnection Conn = new SqlCeConnection(@"Data Source=C:\Users\achinbha\SkyDrive\Code-Project\Str2RegEx\Str2RegEx\Sampler.sdf");
+            Conn.Open();
+            SqlCeCommand Command = new SqlCeCommand("Select * from SampleDB WHERE Hash='"+hash+"'", Conn);
+            SqlCeDataReader DataReader = Command.ExecuteReader();
+
+            //If pattern exist in DB, update its frequency
+            // Else insert to DB
+            if(DataReader.Read())
+            {
+                lblCluster.Text= "Cluster: "+ DataReader.GetString(0);
+                lblHash.Text = "Hash: " + DataReader.GetString(1);
+                lblFrequency.Text = "Frequency: " + DataReader[2].ToString();
+
+                SqlCeDataAdapter Adapter = new SqlCeDataAdapter(Command);
+                Adapter.UpdateCommand = new SqlCeCommand("UPDATE SampleDB SET Frequency=" + (Convert.ToInt32(DataReader[2].ToString()) + 1) + "WHERE Hash = '"+hash+"'", Conn);
+                Adapter.UpdateCommand.ExecuteNonQuery();
+            }
+            else
+            {
+                lblCluster.Text = "Cluster: " + Cluster;
+                lblHash.Text = "Hash: " + hash;
+
+                //
+                // TODO:
+                // Check with user for Cluster name if not provided
+                //
+                SqlCeDataAdapter Adapter = new SqlCeDataAdapter(Command);
+                Adapter.UpdateCommand = new SqlCeCommand("Insert into SampleDB (Cluster, Hash, Frequency, Stack) Values ('"+Cluster+"', '"+hash+"', 0, '')", Conn);
+                lblFrequency.Text = "Frequency: " + Adapter.UpdateCommand.ExecuteNonQuery();
+            }
+            dataGridView1.Refresh();
+            DataReader.Close();
+            Conn.Close();
+
+            bool flag = false;
+
+            for(int i = 0; i < dataGridView1.Rows.Count-1; i++)
+            {
+                if (dataGridView1.Rows[i].Cells[1].Value.ToString() == hash)
+                {
+                    dataGridView1.ClearSelection();
+                    flag = true;
+                    int Frequency = Convert.ToInt32(dataGridView1.Rows[i].Cells[2].Value.ToString());
+                    Frequency = Frequency+1;
+                    dataGridView1.Rows[i].Cells[2].Value = Frequency;
+                    dataGridView1.Rows[i].Selected = true;
+                    break;
+                }
+            }
+
+            if (flag == false)
+            {
+                //dataGridView1.Rows.Add(hash, 0, Cluster,"");
+            }
+            
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: This line of code loads data into the 'samplerDataSet.SampleDB' table. You can move, or remove it, as needed.
+            this.sampleDBTableAdapter.Fill(this.samplerDataSet.SampleDB);
+            
+        }
+
+        private void Populate(String DirPath, String Cluster)
+        {
+            DirectoryInfo Di = new DirectoryInfo(DirPath);
+
+            try
+            {
+                DirectoryInfo[] DirList = Di.GetDirectories();
+                FileInfo[] FileList = Di.GetFiles(@"*.log");
+
+                foreach (FileInfo Fi in FileList)
+                    MineThroughText(Fi.FullName, Cluster);
+
+                foreach (DirectoryInfo Dr in DirList)
+                    Populate(Dr.FullName, Cluster);
+            }
+            catch (Exception ex)
+            {
+                string Msg = ex.Message;
+            }
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            btnTrain.Enabled = checkBox1.Checked;
+        }
+
+        private void btnTrain_Click(object sender, EventArgs e)
+        {
+            //Populate(@"\\automata\Logs\Sampling\Cluster", "Cluster");
+            Populate(@"\\automata\Logs\Sampling\NetLogon", "NetLogon");
+        }
+    }
+}
